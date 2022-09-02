640cd93434f2c70015f4a3aa9bf7056f704382d4
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2022-09-02 09:29:01 +0200

    TodoForm widget: prepre for working in widget creation too (2

--- test/widget_tests/todo_form_test.dart
+++ test/widget_tests/todo_form_test.dart
@@ -35,143 +35,149 @@ main() {
   late TodosNotifier todosNotifier;
   late ProviderScope widgetInSkeletonInProviderScope;
   late Widget widgetInSkeleton;
-  setUp(() {
-    todosNotifier = TodosNotifier(todos);
-    widgetInSkeleton = createWidgetInASkeleton(TodoForm(
-        goRouterContextPopFunctionCall,
-        id: todo2.id,
-        title: todo2.title,
-        description: todo2.description,
-        done: todo2.done));
-    widgetInSkeletonInProviderScope = ProviderScope(
-        overrides: [todosProvider.overrideWithValue(todosNotifier)],
-        child: widgetInSkeleton);
-    reset(goRouterContextPopFunctionCall);
-  });
-  testWidgets("Test the presense of the main widgets",
-      (WidgetTester tester) async {
-    await tester.pumpWidget(widgetInSkeletonInProviderScope);
-    expect(find.byType(Form), findsOneWidget);
-    expect(find.byType(Column), findsOneWidget);
-    final TextFormField titleTextFormField =
-        tester.widget(find.byType(TextFormField).at(0));
-    final TextField titleTextField =
-        tester.widget(find.byType(TextField).at(0));
-    expect(
-        (titleTextField.decoration!.label as Text).data, TodoForm.labelString);
-    expect(titleTextField.keyboardType, TextInputType.text);
-    expect(titleTextFormField.initialValue, todos[1].title);
-    final TextFormField descriptionTextFormField =
-        tester.widget(find.byType(TextFormField).at(1));
-    final TextField descriptionTextField =
-        tester.widget(find.byType(TextField).at(1));
-    expect((descriptionTextField.decoration!.label as Text).data,
-        TodoForm.descriptionString);
-    expect(descriptionTextField.keyboardType, TextInputType.multiline);
-    expect(descriptionTextField.maxLines, 5);
-    expect(descriptionTextFormField.initialValue, todos[1].description);
-    final Checkbox doneCheckbox = tester.widget(find.byType(Checkbox));
-    expect(doneCheckbox.value, todos[1].done);
-    final TextButton submissionButton = tester.widget(find.byType(TextButton));
-    expect((submissionButton.child as Text).data, buttonText);
-    expect(tester.widgetList(find.bySubtype()).toList(),
-        FirstPrecedesSecond(doneCheckbox, titleTextFormField));
-    expect(tester.widgetList(find.bySubtype()).toList(),
-        FirstPrecedesSecond(titleTextFormField, descriptionTextFormField));
-    expect(tester.widgetList(find.bySubtype()).toList(),
-        FirstPrecedesSecond(descriptionTextFormField, submissionButton));
-  });
-  group("Test form validation", () {
-    testWidgets("Testing the validation of the title text field",
+  group("Updating", () {
+    setUp(() {
+      todosNotifier = TodosNotifier(todos);
+      widgetInSkeleton = createWidgetInASkeleton(TodoForm(
+          goRouterContextPopFunctionCall,
+          id: todo2.id,
+          title: todo2.title,
+          description: todo2.description,
+          done: todo2.done));
+      widgetInSkeletonInProviderScope = ProviderScope(
+          overrides: [todosProvider.overrideWithValue(todosNotifier)],
+          child: widgetInSkeleton);
+      reset(goRouterContextPopFunctionCall);
+    });
+    testWidgets("Test the presense of the main widgets",
         (WidgetTester tester) async {
       await tester.pumpWidget(widgetInSkeletonInProviderScope);
-      final submissionTextButtonFinder = find.byType(TextButton);
-      final titleTextFormFieldFinder = find.byType(TextFormField).at(0);
-      await tester.enterText(titleTextFormFieldFinder, " ");
-      await tester.tap(submissionTextButtonFinder);
-      await tester.pumpAndSettle();
-      expect(find.text(TodoForm.titleValidationErrorMessage), findsOneWidget);
-      expect(
-          tester.widgetList(find.bySubtype()).toList(),
-          FirstPrecedesSecond(tester.widget(titleTextFormFieldFinder),
-              tester.widget(find.text(TodoForm.titleValidationErrorMessage))));
-      await tester.enterText(titleTextFormFieldFinder, " f");
-      await tester.tap(submissionTextButtonFinder);
-      await tester.pumpAndSettle();
-      expect(find.text(TodoForm.titleValidationErrorMessage), findsNothing);
+      expect(find.byType(Form), findsOneWidget);
+      expect(find.byType(Column), findsOneWidget);
+      final TextFormField titleTextFormField =
+          tester.widget(find.byType(TextFormField).at(0));
+      final TextField titleTextField =
+          tester.widget(find.byType(TextField).at(0));
+      expect((titleTextField.decoration!.label as Text).data,
+          TodoForm.labelString);
+      expect(titleTextField.keyboardType, TextInputType.text);
+      expect(titleTextFormField.initialValue, todos[1].title);
+      final TextFormField descriptionTextFormField =
+          tester.widget(find.byType(TextFormField).at(1));
+      final TextField descriptionTextField =
+          tester.widget(find.byType(TextField).at(1));
+      expect((descriptionTextField.decoration!.label as Text).data,
+          TodoForm.descriptionString);
+      expect(descriptionTextField.keyboardType, TextInputType.multiline);
+      expect(descriptionTextField.maxLines, 5);
+      expect(descriptionTextFormField.initialValue, todos[1].description);
+      final Checkbox doneCheckbox = tester.widget(find.byType(Checkbox));
+      expect(doneCheckbox.value, todos[1].done);
+      final TextButton submissionButton =
+          tester.widget(find.byType(TextButton));
+      expect((submissionButton.child as Text).data, buttonText);
+      expect(tester.widgetList(find.bySubtype()).toList(),
+          FirstPrecedesSecond(doneCheckbox, titleTextFormField));
+      expect(tester.widgetList(find.bySubtype()).toList(),
+          FirstPrecedesSecond(titleTextFormField, descriptionTextFormField));
+      expect(tester.widgetList(find.bySubtype()).toList(),
+          FirstPrecedesSecond(descriptionTextFormField, submissionButton));
     });
-    testWidgets("Testing the validation of the description text field",
+    group("Test form validation", () {
+      testWidgets("Testing the validation of the title text field",
+          (WidgetTester tester) async {
+        await tester.pumpWidget(widgetInSkeletonInProviderScope);
+        final submissionTextButtonFinder = find.byType(TextButton);
+        final titleTextFormFieldFinder = find.byType(TextFormField).at(0);
+        await tester.enterText(titleTextFormFieldFinder, " ");
+        await tester.tap(submissionTextButtonFinder);
+        await tester.pumpAndSettle();
+        expect(find.text(TodoForm.titleValidationErrorMessage), findsOneWidget);
+        expect(
+            tester.widgetList(find.bySubtype()).toList(),
+            FirstPrecedesSecond(
+                tester.widget(titleTextFormFieldFinder),
+                tester
+                    .widget(find.text(TodoForm.titleValidationErrorMessage))));
+        await tester.enterText(titleTextFormFieldFinder, " f");
+        await tester.tap(submissionTextButtonFinder);
+        await tester.pumpAndSettle();
+        expect(find.text(TodoForm.titleValidationErrorMessage), findsNothing);
+      });
+      testWidgets("Testing the validation of the description text field",
+          (tester) async {
+        await tester.pumpWidget(widgetInSkeletonInProviderScope);
+        final submissionTextButtonFinder = find.byType(TextButton);
+        final descriptionTextFormFieldFinder = find.byType(TextFormField).at(1);
+        await tester.enterText(descriptionTextFormFieldFinder, " ");
+        await tester.tap(submissionTextButtonFinder);
+        await tester.pumpAndSettle();
+        expect(find.text(TodoForm.descriptionValidationErrorMessage),
+            findsOneWidget);
+        expect(
+            tester.widgetList(find.bySubtype()).toList(),
+            FirstPrecedesSecond(
+                tester.widget(descriptionTextFormFieldFinder),
+                tester.widget(
+                    find.text(TodoForm.descriptionValidationErrorMessage))));
+        await tester.enterText(descriptionTextFormFieldFinder, "h ");
+        await tester.tap(submissionTextButtonFinder);
+        await tester.pumpAndSettle();
+        expect(find.text(TodoForm.descriptionValidationErrorMessage),
+            findsNothing);
+      });
+      testWidgets("Testing the validation when no text in the two text fields",
+          (tester) async {
+        await tester.pumpWidget(widgetInSkeletonInProviderScope);
+        final submissionTextButtonFinder = find.byType(TextButton);
+        final titleTextFormFieldFinder = find.byType(TextFormField).at(0);
+        final descriptionTextFormFieldFinder = find.byType(TextFormField).at(1);
+        await tester.enterText(titleTextFormFieldFinder, " ");
+        await tester.enterText(descriptionTextFormFieldFinder, " ");
+        await tester.tap(submissionTextButtonFinder);
+        await tester.pumpAndSettle();
+        expect(
+            tester.widgetList(find.bySubtype()).toList(),
+            FirstPrecedesSecond(
+                tester.widget(find.text(TodoForm.titleValidationErrorMessage)),
+                tester.widget(descriptionTextFormFieldFinder)));
+        expect(
+            tester.widgetList(find.bySubtype()).toList(),
+            FirstPrecedesSecond(
+                tester.widget(descriptionTextFormFieldFinder),
+                tester.widget(
+                    find.text(TodoForm.descriptionValidationErrorMessage))));
+      });
+      testWidgets("The checkbox changes its value when clicked",
+          (tester) async {
+        await tester.pumpWidget(widgetInSkeletonInProviderScope);
+        final doneCheckboxFinder = find.byType(Checkbox);
+        await tester.tap(doneCheckboxFinder);
+        await tester.pumpAndSettle();
+        expect((doneCheckboxFinder.evaluate().first.widget as Checkbox).value,
+            !todos[1].done);
+        await tester.tap(doneCheckboxFinder);
+        await tester.pumpAndSettle();
+        expect((doneCheckboxFinder.evaluate().first.widget as Checkbox).value,
+            todos[1].done);
+      });
+    });
+    testWidgets(
+        "The submission function get called when the text button is being clicked",
         (tester) async {
+      const title = "my title";
+      const description = "my description";
       await tester.pumpWidget(widgetInSkeletonInProviderScope);
-      final submissionTextButtonFinder = find.byType(TextButton);
-      final descriptionTextFormFieldFinder = find.byType(TextFormField).at(1);
-      await tester.enterText(descriptionTextFormFieldFinder, " ");
-      await tester.tap(submissionTextButtonFinder);
+      await tester.enterText(find.byType(TextFormField).at(0), title);
+      await tester.enterText(find.byType(TextFormField).at(1), description);
+      await tester.tap(find.byType(Checkbox));
+      await tester.tap(find.byType(TextButton));
       await tester.pumpAndSettle();
-      expect(find.text(TodoForm.descriptionValidationErrorMessage),
-          findsOneWidget);
-      expect(
-          tester.widgetList(find.bySubtype()).toList(),
-          FirstPrecedesSecond(
-              tester.widget(descriptionTextFormFieldFinder),
-              tester.widget(
-                  find.text(TodoForm.descriptionValidationErrorMessage))));
-      await tester.enterText(descriptionTextFormFieldFinder, "h ");
-      await tester.tap(submissionTextButtonFinder);
-      await tester.pumpAndSettle();
-      expect(
-          find.text(TodoForm.descriptionValidationErrorMessage), findsNothing);
+      expect(todosNotifier.state[1].title, title);
+      expect(todosNotifier.state[1].description, description);
+      expect(todosNotifier.state[1].done, !todo2.done);
+      expect(todosNotifier.state[1].id, todo2.id);
+      verify(goRouterContextPopFunctionCall()).called(1);
     });
-    testWidgets("Testing the validation when no text in the two text fields",
-        (tester) async {
-      await tester.pumpWidget(widgetInSkeletonInProviderScope);
-      final submissionTextButtonFinder = find.byType(TextButton);
-      final titleTextFormFieldFinder = find.byType(TextFormField).at(0);
-      final descriptionTextFormFieldFinder = find.byType(TextFormField).at(1);
-      await tester.enterText(titleTextFormFieldFinder, " ");
-      await tester.enterText(descriptionTextFormFieldFinder, " ");
-      await tester.tap(submissionTextButtonFinder);
-      await tester.pumpAndSettle();
-      expect(
-          tester.widgetList(find.bySubtype()).toList(),
-          FirstPrecedesSecond(
-              tester.widget(find.text(TodoForm.titleValidationErrorMessage)),
-              tester.widget(descriptionTextFormFieldFinder)));
-      expect(
-          tester.widgetList(find.bySubtype()).toList(),
-          FirstPrecedesSecond(
-              tester.widget(descriptionTextFormFieldFinder),
-              tester.widget(
-                  find.text(TodoForm.descriptionValidationErrorMessage))));
-    });
-    testWidgets("The checkbox changes its value when clicked", (tester) async {
-      await tester.pumpWidget(widgetInSkeletonInProviderScope);
-      final doneCheckboxFinder = find.byType(Checkbox);
-      await tester.tap(doneCheckboxFinder);
-      await tester.pumpAndSettle();
-      expect((doneCheckboxFinder.evaluate().first.widget as Checkbox).value,
-          !todos[1].done);
-      await tester.tap(doneCheckboxFinder);
-      await tester.pumpAndSettle();
-      expect((doneCheckboxFinder.evaluate().first.widget as Checkbox).value,
-          todos[1].done);
-    });
-  });
-  testWidgets(
-      "The submission function get called when the text button is being clicked",
-      (tester) async {
-    const title = "my title";
-    const description = "my description";
-    await tester.pumpWidget(widgetInSkeletonInProviderScope);
-    await tester.enterText(find.byType(TextFormField).at(0), title);
-    await tester.enterText(find.byType(TextFormField).at(1), description);
-    await tester.tap(find.byType(Checkbox));
-    await tester.tap(find.byType(TextButton));
-    await tester.pumpAndSettle();
-    expect(todosNotifier.state[1].title, title);
-    expect(todosNotifier.state[1].description, description);
-    expect(todosNotifier.state[1].done, !todo2.done);
-    expect(todosNotifier.state[1].id, todo2.id);
-    verify(goRouterContextPopFunctionCall()).called(1);
   });
 }
