ee1d4b1b846106247445f5484c40cd8c6ea0bd39
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2022-09-04 19:24:42 +0200

    Create AuthState class and ApplicationLoginState enum

---
+++ lib/domain_model/auth_state.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+import 'value_classes/application_login_state.dart';
+
+part 'auth_state.freezed.dart';
+
+@freezed
+class AuthState with _$AuthState {
+  const factory AuthState(
+      {required ApplicationLoginState applicationLoginState,
+      String? email}) = _AuthState;
+}
