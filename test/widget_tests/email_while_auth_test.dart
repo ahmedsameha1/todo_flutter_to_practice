156e8dbd32771b3eea712019a4f035485eb9895c
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2022-09-07 12:03:26 +0200

    EmailWhileAuth widget planned tests

---
+++ test/widget_tests/email_while_auth_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+      "Test the precense of the main widgets", (WidgetTester tester) async {
+        // Form
+        // Column
+        // TextFormField form email
+        // Button for next
+        // Button for cancel
+      });
+
+  testWidgets(
+      "Test the TextFormField validation", (WidgetTester tester) async {
+        // email should be valid using regex
+      });
+
+  testWidgets("Test that next Button call the next action function",
+      (WidgetTester tester) async {});
+
+  testWidgets("Test that cancel Button call the cancel action function",
+      (WidgetTester tester) async {});
+}
