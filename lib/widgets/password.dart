c89737fc5034fcf51337c523885430ef95ffa4a4
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2022-09-20 00:02:14 +0200

    Password widget: Test password textfield validation

--- lib/widgets/password.dart
+++ lib/widgets/password.dart
@@ -3,11 +3,13 @@ import 'package:todo_flutter_to_practice/widgets/register.dart';
 
 class Password extends StatelessWidget {
   final String _email;
-  const Password(this._email, {Key? key}) : super(key: key);
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  Password(this._email, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(children: [
         Text(_email),
         TextFormField(
@@ -18,11 +20,20 @@ class Password extends StatelessWidget {
           obscureText: true,
           autocorrect: false,
           enableSuggestions: false,
+          validator: (value) {
+            if (value == null ||
+                value.trim().length < Register.passwordMinimumLength) {
+              return Register.passwordValidationErrorString;
+            }
+            return null;
+          },
         ),
         Row(
           children: [
             TextButton(
-              onPressed: null,
+              onPressed: () {
+                _formKey.currentState!.validate();
+              },
               child: Text(Register.nextString),
             ),
             TextButton(
--- test/widget_tests/password_test.dart
+++ test/widget_tests/password_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todo_flutter_to_practice/widgets/password.dart';
@@ -8,9 +10,13 @@ import 'skeleton_for_widget_testing.dart';
 
 void main() {
   const email = "test@test.com";
+  late Widget widgetInSkeleton;
+  setUp(() {
+    widgetInSkeleton = createWidgetInASkeleton(Password(email));
+  });
   testWidgets("Test the precense of the main widgets",
       (WidgetTester tester) async {
-    await tester.pumpWidget(createWidgetInASkeleton(const Password(email)));
+    await tester.pumpWidget(widgetInSkeleton);
     final passwordFinder = find.byType(Password);
     expect(passwordFinder, findsOneWidget);
     expect(find.descendant(of: passwordFinder, matching: formFinder),
@@ -47,5 +53,33 @@ void main() {
         ((tester.widget(cancelTextButtonFinder) as TextButton).child as Text)
             .data,
         Register.cancelString);
+  });
+  testWidgets("Test password textfield validation",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(widgetInSkeleton);
+    final passwordTextFieldFinder = textFieldFinder.at(0);
+    await tester.enterText(passwordTextFieldFinder, "8*prt&3k");
+    final nextTextButtonFinder = textButtonFinder.at(0);
+    await tester.tap(nextTextButtonFinder);
+    await tester.pumpAndSettle();
+    final passwordValidationErrorTextFinder = find.descendant(
+        of: textFormFieldFinder.at(0),
+        matching: find.text(Register.passwordValidationErrorString));
+    expect(passwordValidationErrorTextFinder, findsNothing);
+    await tester.enterText(passwordTextFieldFinder, "");
+    await tester.tap(nextTextButtonFinder);
+    await tester.pumpAndSettle();
+    expect(passwordValidationErrorTextFinder, findsOneWidget);
+    await tester.enterText(passwordTextFieldFinder, " ");
+    await tester.tap(nextTextButtonFinder);
+    await tester.pumpAndSettle();
+    final TextField passwordTextField = tester.widget(passwordTextFieldFinder);
+    expect(passwordTextField.controller!.text, "");
+    expect(passwordValidationErrorTextFinder, findsOneWidget);
+    await tester.enterText(passwordTextFieldFinder, " gfh");
+    await tester.tap(nextTextButtonFinder);
+    await tester.pumpAndSettle();
+    expect(passwordTextField.controller!.text, "gfh");
+    expect(passwordValidationErrorTextFinder, findsOneWidget);
   });
 }
