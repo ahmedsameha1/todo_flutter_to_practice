926fab480aadd2b5e986d4e3be756d72d855a926
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2022-11-10 21:23:10 +0200

    MainScreen widget: emailAddress state

--- lib/widgets/main_screen.dart
+++ lib/widgets/main_screen.dart
@@ -3,7 +3,10 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_flutter_to_practice/domain_model/value_classes/application_login_state.dart';
 import 'package:todo_flutter_to_practice/state/notifiers.dart';
+
+import 'email.dart';
 
 class MainScreen extends ConsumerWidget {
   static const String signInUpString = "Sign in/up";
@@ -12,10 +15,23 @@ class MainScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final authStateNotifier = ref.read(authStateNotifierProvider);
+    final authState = ref.watch(authStateProvider);
+    final body;
+    switch (authState.applicationLoginState) {
+      case ApplicationLoginState.loggedOut:
+        body = TextButton(
+            onPressed: authStateNotifier.startLoginFlow,
+            child: const Text(signInUpString));
+        break;
+      case ApplicationLoginState.emailAddress:
+        body =
+            Email(authStateNotifier.verifyEmail, authStateNotifier.toLoggedOut);
+        break;
+      default:
+        body = Text("");
+    }
     return Scaffold(
-        body: TextButton(
-      onPressed: authStateNotifier.startLoginFlow,
-      child: const Text(signInUpString),
-    ));
+      body: body,
+    );
   }
 }
--- test/widget_tests/main_screen_test.dart
+++ test/widget_tests/main_screen_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,24 +9,29 @@ import 'package:mockito/mockito.dart';
 import 'package:todo_flutter_to_practice/domain_model/auth_state.dart';
 import 'package:todo_flutter_to_practice/domain_model/value_classes/application_login_state.dart';
 import 'package:todo_flutter_to_practice/state/auth_state_notifier.dart';
+import 'package:todo_flutter_to_practice/widgets/email.dart';
 import 'package:todo_flutter_to_practice/widgets/main_screen.dart';
 import 'package:todo_flutter_to_practice/state/notifiers.dart';
 
+import '../state/auth_state_notifier_test.mocks.dart';
 import 'common_finders.dart';
-import 'main_screen_test.mocks.dart';
 
-@GenerateMocks([AuthStateNotifier])
+@GenerateMocks([FirebaseAuth])
 void main() {
   late AuthStateNotifier authStateNotifier;
+  late StreamController<User?> streamController;
+  late FirebaseAuth firebaseAuth;
   setUp(() {
-    authStateNotifier = MockAuthStateNotifier();
+    streamController = StreamController();
+    firebaseAuth = MockFirebaseAuth();
+    when(firebaseAuth.userChanges()).thenAnswer((_) => streamController.stream);
   });
   testWidgets("loggedOut state case", (WidgetTester tester) async {
+    authStateNotifier = AuthStateNotifier(firebaseAuth,
+        AuthState(applicationLoginState: ApplicationLoginState.loggedOut));
     await tester.pumpWidget(ProviderScope(overrides: [
       authStateNotifierProvider.overrideWithValue(authStateNotifier)
     ], child: MaterialApp(home: MainScreen())));
-    when(authStateNotifier.state).thenReturn(
-        AuthState(applicationLoginState: ApplicationLoginState.loggedOut));
     Scaffold scaffold = tester.widget(scaffoldFinder);
     expect(scaffold.body.runtimeType, TextButton);
     expect(find.descendant(of: scaffoldFinder, matching: textButtonFinder),
@@ -32,5 +39,21 @@ void main() {
     TextButton textButton = tester.widget(textButtonFinder);
     expect((textButton.child as Text).data, MainScreen.signInUpString);
     expect(textButton.onPressed, authStateNotifier.startLoginFlow);
+  });
+
+  testWidgets("emailAddress state case", (WidgetTester tester) async {
+    authStateNotifier = AuthStateNotifier(firebaseAuth,
+        AuthState(applicationLoginState: ApplicationLoginState.emailAddress));
+    await tester.pumpWidget(ProviderScope(overrides: [
+      authStateNotifierProvider.overrideWithValue(authStateNotifier)
+    ], child: MaterialApp(home: MainScreen())));
+    Scaffold scaffold = tester.widget(scaffoldFinder);
+    expect(scaffold.body.runtimeType, Email);
+    final emailFinder = find.byType(Email);
+    expect(find.descendant(of: scaffoldFinder, matching: emailFinder),
+        findsOneWidget);
+    Email emailWidget = tester.widget(emailFinder);
+    expect(emailWidget.nextAction, authStateNotifier.verifyEmail);
+    expect(emailWidget.cancelAction, authStateNotifier.toLoggedOut);
   });
 }
